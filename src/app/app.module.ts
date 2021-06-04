e5800c3d5077f6e8466173152c99f50a8d8f29bc
Matheus Salomão <mathesbh@gmail.com>
2021-06-04 15:12:02 -0300

    Instalando e configurando bootstrap4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieItemComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
